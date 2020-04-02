528739f348afda6965854d3310289a40faec0827
soumiksamanta007 <soumiksamantaa@gmail.com>
2020-04-02 11:13:27 +0530

    updated corona_diff.py and added results

--- corona_diff.py
+++ corona_diff.py
@@ -1,27 +1,31 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Mar 30 13:05:45 2020
+Created on Wed Apr  1 17:01:03 2020
 
-@author: paul
+@author: soumik
 """
-# A SIR model for the spread of Covid-19 virus
+# A SIR model for the spread of virus
 
 import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 from pylab import *
 
-N=10000      # Initial population
+N=10000     # Initial population
 I=1         # Initially infected people
 R=0         # 0 Recovered
 S=N-I-R     # Susceptible (N=S+I+R)
 
-beta=4.5         # Rate of infection
-gamma=.5           # Rate of recovery
-    
+radius = 1  # Radius of infection
+prob = 0.2  # Probability of Infection
+T=14        # Time-play
 
-T=14       # Time-play
+omega = 1                                               # Rate of social distancing
+gamma = .5                                              # Rate of recovery
+beta = 2.5           									# Reproduction Number
+beta2 = radius*prob*T*(beta/(beta + omega + gamma))     # Rate of infection
+    
 
 def SIR(vals,t,b,g):
     S,I,R=vals
@@ -33,9 +37,9 @@ def SIR(vals,t,b,g):
     return [dS,dI,dR]
 
     
-t=np.linspace(0,T,1000)
+t=np.linspace(0,4*T,1000)
 x=[S,I,R]           # Initial params
-rates=(beta,gamma)
+rates=(beta2,gamma)
 sol=odeint(SIR,x,t,args=rates)
 
 # Plot ODE solutions
@@ -46,7 +50,6 @@ plt.plot(t,sol[:,2])
 plt.xlabel('Time')
 plt.ylabel('Population')
 plt.legend(['S','I','R'],shadow=True)
-plt.title('COVID')
 plt.draw()
-savefig("/home/paul/Documents/COVID/"+"diff_b"+str(beta)+"_g"+str(gamma)+".png",dpi=400)
+savefig("/home/soumik/Desktop/covid-19/results/"+"diff_radius"+str(radius)+"_prob"+str(prob)+"_omega"+str(omega)+".png",dpi=400)
 
